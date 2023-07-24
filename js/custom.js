4cc6e4f7b458da3bae7e60bbb9a17b69e97735d8
andry2327 <andry2327@gmail.com>
2023-07-24 16:29:17 +0200

    commit project scrolling

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,16 @@
 function div_selectById() {
     console.log("weeelaaaaaa")
 }
+
+const scrollLeftButton = document.querySelector(".scroll-left");
+const scrollRightButton = document.querySelector(".scroll-right");
+const mediaScroller = document.querySelector(".media-scroller");
+const scrollAmount = 200; // Adjust the amount of scrolling
+
+scrollLeftButton.addEventListener("click", () => {
+  mediaScroller.scrollBy({ left: scrollAmount, behavior: "smooth" });
+});
+
+scrollRightButton.addEventListener("click", () => {
+  mediaScroller.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+});
